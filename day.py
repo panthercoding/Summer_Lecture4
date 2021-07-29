ad6793c36d72f0f114abcce8892a3d64d5fe991f
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-07-29 17:15:37 -0500

    Create day.py

---
+++ day.py
@@ -0,0 +1,16 @@
+import sys
+
+def dayOfWeek(month,day,year):
+  """ fill code"""
+  return(0)
+
+def main():
+  month = int(sys.argv[1])
+  day = int(sys.argv[2])
+  year = int(sys.argv[3])
+
+  D = dayOfWeek(month,day,year)
+  
+  """ fill code """
+
+main()
