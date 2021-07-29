14626a5e0c975fefb66388efb0c73012f8784545
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-07-29 16:46:26 -0500

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+import sys
+
+def extractAlphabetic(string):
+  """ fill code here """
+  return(0)
+
+def isPalindrome(string):
+  """ fill code here """
+  return(0)
+
+print(isPalindrome(extractCharacters(sys.argv[1])))
